4d681e94ca537c54b4b61d212b79ec7a7bafe9b7
“yang-samuel.lu” <yang-samuel.lu@schneider-electric.com>
2017-04-27 21:30:33 +0800

    add sharemodule to local

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #echo run build
 rm ../build_file/*.apk
 
+cp ./android/app/src/main/java/com/energymost/hipdiscoing/ShareModule.java ./node_modules/react-native/ReactAndroid/src/main/java/com/facebook/react/modules/share/ShareModule.java
+
 #npm install
 cd android && ./gradlew assembleRelease
 cd ..
