3f155722694394c0b375aa90981b2b69bef95685
“yang-samuel.lu” <yang-samuel.lu@schneider-electric.com>
2017-05-08 15:35:14 +0800

    modify login textinput

--- app/components/login/Form.js
+++ app/components/login/Form.js
@@ -40,7 +40,7 @@ export default class Form extends Component{
     }
     var inputStyle={};
     if (Platform.OS==='android') {
-      inputStyle={marginTop:8,};
+      // inputStyle={marginTop:8,};
     }
     return (
       <View style={styles.form}>
@@ -54,7 +54,7 @@ export default class Form extends Component{
               underlineColorAndroid={'transparent'}
               textAlign={'left'}
               placeholderTextColor="lightgray"
-              textAlignVertical={'bottom'}
+              textAlignVertical={'center'}
               placeholder={"请填写11位手机号码"}
               onChangeText={(text)=>this._codeChanged('phoneNumber',text)}
               value={this.props.data.get('phoneNumber')}
@@ -70,7 +70,7 @@ export default class Form extends Component{
               underlineColorAndroid={'transparent'}
               textAlign={'left'}
               placeholderTextColor="lightgray"
-              textAlignVertical={'bottom'}
+              textAlignVertical={'center'}
               placeholder={"请填写4位验证码"}
               onChangeText={(text)=>this._codeChanged('validCode',text)}
               value={this.props.data.get('validCode')}
@@ -91,7 +91,7 @@ export default class Form extends Component{
   _getPasswordValidationForm(){
     var inputStyle={};
     if (Platform.OS==='android') {
-      inputStyle={marginTop:8,};
+      // inputStyle={marginTop:18,};
     }
     return (
       <View style={styles.form}>
@@ -120,7 +120,7 @@ export default class Form extends Component{
               underlineColorAndroid={'transparent'}
               textAlign={'left'}
               placeholderTextColor="lightgray"
-              textAlignVertical={'bottom'}
+              textAlignVertical={'center'}
               placeholder={"请填写密码"}
               onChangeText={(text)=>this._codeChanged("password",text)}
               value={this.props.data.get('password')}
